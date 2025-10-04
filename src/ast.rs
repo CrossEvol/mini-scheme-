d6364a0543a90c0e3f5d0f1bc370a8a1b5940c74
CrossEvol <296662402@qq.com>
2025-10-04 20:23:11 +0800

    fix: customize Debug trait for Expr

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,8 @@
 use crate::Token;
+use std::fmt;
 
 /// Main expression type representing all possible Scheme expressions
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub enum Expr {
     // Literals
     Number(f64, Option<Token>),
@@ -40,8 +41,39 @@ pub enum Expr {
     Vector(Vec<Expr>, Option<Token>),
 }
 
+impl fmt::Debug for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Expr::Number(n, _) => write!(f, "Number({})", n),
+            Expr::String(s, _) => write!(f, "String({:?})", s),
+            Expr::Character(c, _) => write!(f, "Character('{}')", c),
+            Expr::Boolean(b, _) => write!(f, "Boolean({})", b),
+            Expr::Variable(name, _) => write!(f, "Variable({:?})", name),
+            Expr::Define(def) => write!(f, "Define({:?})", def),
+            Expr::Lambda(lambda) => write!(f, "Lambda({:?})", lambda),
+            Expr::If(if_expr) => write!(f, "If({:?})", if_expr),
+            Expr::Cond(cond) => write!(f, "Cond({:?})", cond),
+            Expr::Let(let_expr) => write!(f, "Let({:?})", let_expr),
+            Expr::LetStar(let_star) => write!(f, "LetStar({:?})", let_star),
+            Expr::LetLoop(let_loop) => write!(f, "LetLoop({:?})", let_loop),
+            Expr::LetValues(let_values) => write!(f, "LetValues({:?})", let_values),
+            Expr::CallWithValues(cwv) => write!(f, "CallWithValues({:?})", cwv),
+            Expr::Import(import) => write!(f, "Import({:?})", import),
+            Expr::Set(set) => write!(f, "Set({:?})", set),
+            Expr::Call(func, args) => write!(f, "Call({:?}, {:?})", func, args),
+            Expr::Quote(expr, _) => write!(f, "Quote({:?})", expr),
+            Expr::QuasiQuote(expr, _) => write!(f, "QuasiQuote({:?})", expr),
+            Expr::UnQuote(expr, _) => write!(f, "UnQuote({:?})", expr),
+            Expr::UnQuoteSplicing(expr, _) => write!(f, "UnQuoteSplicing({:?})", expr),
+            Expr::Begin(begin) => write!(f, "Begin({:?})", begin),
+            Expr::List(exprs, _) => write!(f, "List({:?})", exprs),
+            Expr::Vector(exprs, _) => write!(f, "Vector({:?})", exprs),
+        }
+    }
+}
+
 /// Define expression: (define name value) or (define (name args...) body...)
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct DefineExpr {
     pub name: String,
     pub value: Expr,
@@ -49,8 +81,18 @@ pub struct DefineExpr {
     pub token: Option<Token>,
 }
 
+impl fmt::Debug for DefineExpr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("DefineExpr")
+            .field("name", &self.name)
+            .field("value", &self.value)
+            .field("docstring", &self.docstring)
+            .finish()
+    }
+}
+
 /// Lambda expression: (lambda (args...) body...)
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct LambdaExpr {
     pub params: Vec<String>,
     pub body: Vec<Expr>,
@@ -58,8 +100,18 @@ pub struct LambdaExpr {
     pub token: Option<Token>,
 }
 
+impl fmt::Debug for LambdaExpr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("LambdaExpr")
+            .field("params", &self.params)
+            .field("body", &self.body)
+            .field("docstring", &self.docstring)
+            .finish()
+    }
+}
+
 /// If expression: (if condition then else)
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct IfExpr {
     pub condition: Expr,
     pub then_expr: Expr,
@@ -67,39 +119,84 @@ pub struct IfExpr {
     pub token: Option<Token>,
 }
 
+impl fmt::Debug for IfExpr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("IfExpr")
+            .field("condition", &self.condition)
+            .field("then_expr", &self.then_expr)
+            .field("else_expr", &self.else_expr)
+            .finish()
+    }
+}
+
 /// Cond expression: (cond (test body...) ... (else body...))
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct CondExpr {
     pub clauses: Vec<CondClause>,
     pub token: Option<Token>,
 }
 
+impl fmt::Debug for CondExpr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("CondExpr")
+            .field("clauses", &self.clauses)
+            .finish()
+    }
+}
+
 /// Individual clause in a cond expression
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct CondClause {
     pub test: Expr,
     pub body: Vec<Expr>,
     pub token: Option<Token>,
 }
 
+impl fmt::Debug for CondClause {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("CondClause")
+            .field("test", &self.test)
+            .field("body", &self.body)
+            .finish()
+    }
+}
+
 /// Let expression: (let ((var val) ...) body...)
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct LetExpr {
     pub bindings: Vec<(String, Expr)>,
     pub body: Vec<Expr>,
     pub token: Option<Token>,
 }
 
+impl fmt::Debug for LetExpr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("LetExpr")
+            .field("bindings", &self.bindings)
+            .field("body", &self.body)
+            .finish()
+    }
+}
+
 /// Let* expression: (let* ((var val) ...) body...)
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct LetStarExpr {
     pub bindings: Vec<(String, Expr)>,
     pub body: Vec<Expr>,
     pub token: Option<Token>,
 }
 
+impl fmt::Debug for LetStarExpr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("LetStarExpr")
+            .field("bindings", &self.bindings)
+            .field("body", &self.body)
+            .finish()
+    }
+}
+
 /// Named let expression: (let loop ((var val) ...) body...)
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct LetLoopExpr {
     pub name: String,
     pub bindings: Vec<(String, Expr)>,
@@ -107,42 +204,95 @@ pub struct LetLoopExpr {
     pub token: Option<Token>,
 }
 
+impl fmt::Debug for LetLoopExpr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("LetLoopExpr")
+            .field("name", &self.name)
+            .field("bindings", &self.bindings)
+            .field("body", &self.body)
+            .finish()
+    }
+}
+
 /// Let-values expression: (let-values (((var ...) expr) ...) body...)
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct LetValuesExpr {
     pub bindings: Vec<(Vec<String>, Expr)>,
     pub body: Vec<Expr>,
     pub token: Option<Token>,
 }
 
+impl fmt::Debug for LetValuesExpr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("LetValuesExpr")
+            .field("bindings", &self.bindings)
+            .field("body", &self.body)
+            .finish()
+    }
+}
+
 /// Call-with-values expression: (call-with-values producer consumer)
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct CallWithValuesExpr {
     pub producer: Expr,
     pub consumer: Expr,
     pub token: Option<Token>,
 }
 
+impl fmt::Debug for CallWithValuesExpr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("CallWithValuesExpr")
+            .field("producer", &self.producer)
+            .field("consumer", &self.consumer)
+            .finish()
+    }
+}
+
 /// Import expression: (import module-spec)
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct ImportExpr {
     pub module_spec: Expr,
     pub token: Option<Token>,
 }
 
+impl fmt::Debug for ImportExpr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("ImportExpr")
+            .field("module_spec", &self.module_spec)
+            .finish()
+    }
+}
+
 /// Begin expression: (begin expression expression  … expression )
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct BeginExpr {
     pub body: Vec<Expr>,
     pub token: Option<Token>,
 }
 
+impl fmt::Debug for BeginExpr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("BeginExpr")
+            .field("body", &self.body)
+            .finish()
+    }
+}
+
 /// Set! expression: (set! var value)
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct SetExpr {
     pub variable: String,
     pub value: Expr,
     pub token: Option<Token>,
+}
+
+impl fmt::Debug for SetExpr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("SetExpr")
+            .field("variable", &self.variable)
+            .field("value", &self.value)
+            .finish()
+    }
 }
 
 impl DefineExpr {
